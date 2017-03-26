f0b94f440e2fb184602d988f4df0ffe95efc564c
Damarhabibie <habibiedamar@gmail.com>
2017-03-26 22:16:43 +0800

    Posttest 4

--- laravel5/app/Matakuliah.php
+++ laravel5/app/Matakuliah.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Matakuliah extends Model
 {
     protected $table = 'matakuliah';
+    protected $fillable = ['title','keterangan'];
+
+    public function dosen_matakuliah()
+    {
+    	return $this->hasMany(Dosen_Matakuliah::class);
+    }
 }
